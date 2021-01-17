49e2b2b495c26d94c6c4d23e6e83530d981a7399
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2021-01-17 21:25:41 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -127,10 +127,168 @@ import java.util.Scanner;
 
 
 
+	 /// Sort based Priority.
+     public static ArrayList<Process> sortPriority(ArrayList<Process> procs) {
+		
+		ArrayList<Integer> Priority = new ArrayList<Integer>();
+		ArrayList<Process> temp = new ArrayList<Process>();
+		
+		// getting all the arrival times
+		for (Process proc : procs) {
+			Priority.add(proc.getPriorityNumber());
+		}
+		
+		// sorting Priority
+		Collections.sort(Priority);
+		
+		// storing the sorted processes in temp
+		for (Integer Priority_Value : Priority) {
+			for (int i = 0; i < procs.size(); i++) {
+				
+				if (procs.get(i).getPriorityNumber() == Priority_Value) {
+					temp.add(procs.get(i));
+					procs.remove(i);
+
+					i--;
+				}
+			}
+		}
+		
+		procs = temp;
+		return procs;
+	}
+     
+   /// Sort based Process Name.
+     public static ArrayList<Process> sortProcessName(ArrayList<Process> procs) {
+		
+		ArrayList<String> ProcName = new ArrayList<String>();
+		ArrayList<Process> temp = new ArrayList<Process>();
+		
+		// getting all the arrival times
+		for (Process proc : procs) {
+			ProcName.add(proc.ProcessName);
+		}
+		
+		// sorting Priority
+		Collections.sort(ProcName);
+		
+		// storing the sorted processes in temp
+		for (String ProcessName : ProcName) {
+			for (int i = 0; i < procs.size(); i++) {
+				
+				if (procs.get(i).ProcessName == ProcessName) {
+					temp.add(procs.get(i));
+					procs.remove(i);
+
+					i--;
+				}
+			}
+		}
+		
+		procs = temp;
+		return procs;
+	}
+
+	
+
+	 
+	 
+	 
 	public static void PriorityScheduling() {
 		// TODO Auto-generated method stub
-	        	        
+		
+		ArrayList<Process> myProcs = new ArrayList<Process>(allProcesses);
+		myProcs = sortPriority(myProcs); // sorts myProcs based on Priority.
+		
+		double average_WaitingTime = 0.0,average_Turnaround=0.0;
+		boolean flag = true;
+		int n= myProcs.size();
+		int CompletionTime[] = new int[n];  // complete time
+		int turnaroundTime[] = new int[n];  // turn around time
+		int waitingTime[] = new int[n];  // waiting time
+		
+		/*
+		for(int i=0;i<myProcs.size();++i)
+		{
+			System.out.println(myProcs.get(i).ProcessName  + "    "+ myProcs.get(i).priorityNumber );
+		}*/
+		    
+		     
+			/// Completion Time
+			for(int i=0;i<n;++i)
+			{
+				if(flag)
+				{
+					CompletionTime[i] = myProcs.get(i).BurstTime  ;
+					flag = false;
+				}
+				else
+				{
+						CompletionTime[i] =  CompletionTime[i-1]+myProcs.get(i).BurstTime ;	
+				}		
+			}
+			
+			
+			/// Waiting Time.
+			for(int i=0;i<n;++i)
+			{
+				waitingTime[i] = ( CompletionTime[i] - myProcs.get(i).BurstTime  );
+				average_WaitingTime+=waitingTime[i];
+			}
+			
+			/// Turnaround Time
+			for(int i=0 ; i<n;++i)
+			{
+				turnaroundTime[i] = waitingTime[i] + myProcs.get(i).BurstTime ;
+				average_Turnaround+=turnaroundTime[i];
+			}
+			
+			
+			
+			
+			/// Print Process.
+			
+			System.out.println("Process | Execution Time  | Priority | Arrival Time  | Completion Time | Turnaround Time | Waiting Time ");
+			for(int i=0;i<n;++i)
+			{
+				System.out.print(myProcs.get(i).ProcessName + "		");
+				System.out.print(myProcs.get(i).BurstTime + "		");
+				System.out.print(myProcs.get(i).priorityNumber + "          ");
+				System.out.print(myProcs.get(i).ArrivalTime + "	             ");
+				System.out.print(CompletionTime[i] + "	              ");
+				System.out.print(turnaroundTime[i] + "                ");
+				System.out.print(waitingTime[i]);
+				System.out.println();
+				
+			}
+			
+			System.out.println();
+			System.out.println();
+			
+			myProcs = sortProcessName(myProcs); // sorts myProcs based on Process Name.
+			System.out.println("Process | Execution Time  | Priority | Arrival Time  | Completion Time | Turnaround Time | Waiting Time ");
+			for(int i=0;i<n;++i)
+			{
+				System.out.print(myProcs.get(i).ProcessName + "		");
+				System.out.print(myProcs.get(i).BurstTime + "		");
+				System.out.print(myProcs.get(i).priorityNumber + "          ");
+				System.out.print(myProcs.get(i).ArrivalTime + "	             ");
+				System.out.print(CompletionTime[i] + "	              ");
+				System.out.print(turnaroundTime[i] + "                ");
+				System.out.print(waitingTime[i]);
+				System.out.println();
+				
+			}
+			
+			System.out.println("Average Waiting Time = " + average_WaitingTime/n);
+			System.out.println("Average Turnaround Time = " + average_Turnaround/n);
+			
+			
+			
+		
+		
 	}
+
 
 	public static void printAllProcSchedule() {
 		
@@ -283,8 +441,74 @@ import java.util.Scanner;
 
 
 	public static void SJFScheduling() {
-		// TODO Auto-generated method stub
-		
+		int n= allProcesses.size();
+		String pname[] = new String[n]; // process name
+		int arrivalt[] = new int[n];  // arrival time
+		int burstt[] = new int[n];  // burst time
+		int complett[] = new int[n];  // complete time
+		int turnaround[] = new int[n];  // turn around time
+		int waitingt[] = new int[n];  // waiting time
+		int flag[]  = new int[n];  //  flag to checks process is completed or not
+		int k[]  = new int[n];  // it is also stores burst time
+	    int i, startT=0, tot=0;
+	    float avgwt=0, avgta=0;
+ 
+	    for (i=0;i<n;i++)
+	    {
+	    	pname[i]= allProcesses.get(i).ProcessName;
+	    	arrivalt[i]= allProcesses.get(i).ArrivalTime;
+	    	burstt[i]= allProcesses.get(i).BurstTime;
+	    	k[i]= burstt[i];
+	    	flag[i]= 0;
+	    }
+	    
+	    while(true){
+	    	int min=200,c=n;
+	    	if (tot==n)
+	    		break;
+	    	
+	    	for ( i=0;i<n;i++)
+	    	{
+	    		if ((arrivalt[i]<=startT) && (flag[i]==0) && (burstt[i]<min))
+	    		{	
+	    			min=burstt[i];
+	    			c=i;
+	    		}
+	    	}
+	    	
+	    	if (c==n)
+	    		startT++;
+	    	else
+	    	{
+	    		burstt[c]--;
+	    		System.out.print(pname[c]+ " | ");
+	    		startT++;
+	    		if (burstt[c]==0)
+	    		{
+	    			complett[c]= startT;
+	    			flag[c]=1;
+	    			tot++;
+	    		}
+	    	}
+	    }
+	    
+	    for(i=0;i<n;i++)
+	    {
+	    	turnaround[i] = complett[i] - arrivalt[i];
+	    	waitingt[i] = turnaround[i] - k[i];
+	    	avgwt+= waitingt[i];
+	    	avgta+= turnaround[i];
+	    }
+	    System.out.println();
+	    System.out.println("p_name \t|   arrival \t|   burst \t|  turnaround \t|    waiting");
+	    for(i=0;i<n;i++)
+	    {
+	    	System.out.println(pname[i] +"\t|\t"+ arrivalt[i]+"\t|\t"+ k[i]  +"\t|\t"+ turnaround[i] +"\t|\t"+ waitingt[i]);
+	    }
+	    
+	    System.out.println("\naverage Turnaround time is: "+ (float)(avgta/n));
+	    System.out.println("average Waiting time is: "+ (float)(avgwt/n));
+	   
 	}
 
 
